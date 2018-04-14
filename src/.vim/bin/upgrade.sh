df677b85b6f05e22d744f47300cbde12e51cabea
valsorym <valsorym.e@gmail.com>
2018-04-14 08:39:46 +0300

    Update

--- src/.vim/bin/upgrade.sh
+++ src/.vim/bin/upgrade.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Author: valsorym <i@valsorym.com>
 
+# TODO: Make everything as a list and run in a loop.
 mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/syntax
 mkdir -p ~/.vim/keymap
+mkdir -p ~/.vim/compiler
+mkdir -p ~/.vim/ftdetect
+mkdir -p ~/.vim/ftplugin
+mkdir -p ~/.vim/indent
 
 for dir in `find ~/.vim/bundle -type d`
 do
@@ -14,6 +19,14 @@ do
         cp -Rf $dir/* ~/.vim/syntax/
     elif [ $DIRECTROY == "keymap" ]; then
         cp -Rf $dir/* ~/.vim/keymap/
+    elif [ $DIRECTROY == "compiler" ]; then
+        cp -Rf $dir/* ~/.vim/compiler/
+    elif [ $DIRECTROY == "ftdetect" ]; then
+        cp -Rf $dir/* ~/.vim/ftdetect/
+    elif [ $DIRECTROY == "ftplugin" ]; then
+        cp -Rf $dir/* ~/.vim/ftplugin/
+    elif [ $DIRECTROY == "indent" ]; then
+        cp -Rf $dir/* ~/.vim/indent/
     fi
 done
 
